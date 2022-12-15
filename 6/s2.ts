b7ac31b73ae2966dea8c0cc748d9dfb5b61b272d
RedBlueKoi <bragin.inred@gmail.com>
2022-12-15 11:40:22 +0100

    feat: day 6 challenge done

---
+++ 6/s2.ts
@@ -0,0 +1,27 @@
+export const a = 1
+
+const input = await Deno.readTextFile('./input.txt')
+const bufferSize = 14
+
+const shouldContinue = (buffer: string[]) => {
+  if (buffer.length < bufferSize) return true
+  if (new Set(buffer).size !== buffer.length) return true
+
+  return false
+}
+
+const solution = () => {
+  const buffer: string[] = []
+  let index = 0
+
+  while (shouldContinue(buffer) && index < input.length - 1) {
+    if (buffer.length === bufferSize) buffer.shift()
+    index++
+    const char = input.charAt(index)
+    buffer.push(char)
+  }
+
+  console.log(index + 1)
+}
+
+solution()
--- README.md
+++ README.md
@@ -25,3 +25,4 @@ deno run --allow-read --watch s1.ts
 - [Day 3: Rucksack Reorganization](https://adventofcode.com/2022/day/3)
 - [Day 4: Camp Cleanup](https://adventofcode.com/2022/day/4)
 - [Day 5: Supply Stacks](https://adventofcode.com/2022/day/5)
+- [Day 6: Turning Table](https://adventofcode.com/2022/day/6)
